e99a9c0881f3643d03969c77910c95d9597d6d2e
Abdur Rahman <abdurrahmansoftw@gmail.com>
2026-08-08 01:50:20 +0600

    feat(phase-00): strictly finalize phase 00 requirements

--- apps/portal/next.config.ts
+++ apps/portal/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    const API_URL = process.env.INTERNAL_API_URL || "http://127.0.0.1:8000";
+
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${API_URL}/api/:path*`, // Proxy to FastAPI
+      }
+    ];
+  },
 };
 
 export default nextConfig;
